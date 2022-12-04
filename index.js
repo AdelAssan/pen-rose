5aa27c532a4e12399f197a6ab9dbced2080656a7
AdelAssan <adelassanova@gmail.com>
2022-12-04 12:46:14 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("header__dropdown_active");
+    document.getElementById("arrow").classList.toggle("header__link_arrow_active");
+}
+
